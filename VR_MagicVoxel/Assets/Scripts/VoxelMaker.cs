e29dc61e86576beb459ba0dd08ded7ce35843929
94mark <90877724+94mark@users.noreply.github.com>
2022-01-13 17:52:13 +0900

    Update VoxelMaker.cs
    일정 시간마다 복셀 생성하기

--- VR_MagicVoxel/Assets/Scripts/VoxelMaker.cs
+++ VR_MagicVoxel/Assets/Scripts/VoxelMaker.cs
@@ -14,6 +14,10 @@ public class VoxelMaker : MonoBehaviour
     public int voxelPoolSize = 20;
     //오브젝트 풀
     public static List<GameObject> voxelPool = new List<GameObject>();
+    //생성 시간
+    public float createTime = 0.1f;
+    //경과 시간
+    float currentTime = 0;
 
     void Start()
     {
@@ -28,21 +32,26 @@ public class VoxelMaker : MonoBehaviour
             voxelPool.Add(voxel);
         }
     }
+
     void Update()
     {
-        //사용자가 마우스를 클릭한 지점에 복셀을 1개 만들고 싶다
-        //1. 사용자가 마우스를 클릭했다면
-        if(Input.GetButtonDown("Fire1"))
+        //일정 시간마다 복셀을 만들고 싶다
+        //1. 경과 시간이 흐른다
+        currentTime += Time.deltaTime;
+        //2. 경과 시간이 생성 시간을 초과했다면
+        if(currentTime > createTime)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo = new RaycastHit();
-            //2. 마우스의 위치가 바닥 위에 위치해 있다면
+            //3. Ray를 쏜다
             if (Physics.Raycast(ray, out hitInfo))
             {
                 //복셀 오브젝트 풀 이용하기
                 //1. 만약 오브젝트 풀에 복셀이 있다면
-                if(voxelPool.Count > 0)
+                if (voxelPool.Count > 0)
                 {
+                    //복셀을 생성했을 때만 경과 시간을 초기화해준다
+                    currentTime = 0;
                     //2. 오브젝트 풀에서 복셀을 하나 가져온다
                     GameObject voxel = voxelPool[0];
                     //3. 복셀을 활성화한다
