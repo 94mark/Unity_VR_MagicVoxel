ce6a3afa97819825e0d12127b83d1b3268e96334
94mark <90877724+94mark@users.noreply.github.com>
2022-01-14 23:14:05 +0900

    Update VoxelMaker.cs
    ARAVRInput.Get 함수를 통한 입력 처리
    컨트롤러 방향으로 Ray 생성

--- VR_MagicVoxel/Assets/Scripts/VoxelMaker.cs
+++ VR_MagicVoxel/Assets/Scripts/VoxelMaker.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 //필요 속성 : 복셀 공장
 //오브젝트 풀에 비활성화된 복셀을 담고 싶다
 //필요 속성 : 오브젝트 풀, 오브젝트 풀의 크기
+//VR컨트롤러의 발사 버튼을 누르면 컨트롤러가 향하는 방향으로 복셀을 생성
 public class VoxelMaker : MonoBehaviour
 {
     //복셀 공장
@@ -35,31 +36,37 @@ public class VoxelMaker : MonoBehaviour
 
     void Update()
     {
-        //일정 시간마다 복셀을 만들고 싶다
-        //1. 경과 시간이 흐른다
-        currentTime += Time.deltaTime;
-        //2. 경과 시간이 생성 시간을 초과했다면
-        if(currentTime > createTime)
+        //VR 컨트롤러의 발사 버튼을 누르면
+        if(ARAVRInput.Get(ARAVRInput.Button.One))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo = new RaycastHit();
-            //3. Ray를 쏜다
-            if (Physics.Raycast(ray, out hitInfo))
+            //일정 시간마다 복셀을 만들고 싶다
+            //1. 경과 시간이 흐른다
+            currentTime += Time.deltaTime;
+            //2. 경과 시간이 생성 시간을 초과했다면
+            if (currentTime > createTime)
             {
-                //복셀 오브젝트 풀 이용하기
-                //1. 만약 오브젝트 풀에 복셀이 있다면
-                if (voxelPool.Count > 0)
+                //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                //2) 컨트롤러가 향하는 방향으로 시선 만들기
+                Ray ray = new Ray(ARAVRInput.RHandPositon, ARAVRInput.RHandDirection);
+                RaycastHit hitInfo = new RaycastHit();
+                //3. Ray를 쏜다
+                if (Physics.Raycast(ray, out hitInfo))
                 {
-                    //복셀을 생성했을 때만 경과 시간을 초기화해준다
-                    currentTime = 0;
-                    //2. 오브젝트 풀에서 복셀을 하나 가져온다
-                    GameObject voxel = voxelPool[0];
-                    //3. 복셀을 활성화한다
-                    voxel.SetActive(true);
-                    //4. 복셀을 배치하고 싶다
-                    voxel.transform.position = hitInfo.point;
-                    //5. 오브젝트 풀에서 복셀을 제거한다
-                    voxelPool.RemoveAt(0);
+                    //복셀 오브젝트 풀 이용하기
+                    //1. 만약 오브젝트 풀에 복셀이 있다면
+                    if (voxelPool.Count > 0)
+                    {
+                        //복셀을 생성했을 때만 경과 시간을 초기화해준다
+                        currentTime = 0;
+                        //2. 오브젝트 풀에서 복셀을 하나 가져온다
+                        GameObject voxel = voxelPool[0];
+                        //3. 복셀을 활성화한다
+                        voxel.SetActive(true);
+                        //4. 복셀을 배치하고 싶다
+                        voxel.transform.position = hitInfo.point;
+                        //5. 오브젝트 풀에서 복셀을 제거한다
+                        voxelPool.RemoveAt(0);
+                    }
                 }
             }
         }
